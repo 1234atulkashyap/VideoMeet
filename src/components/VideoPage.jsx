8da086e6b19b180b2651b817879a438cd35655cd
Atul kumar Kashyap <1234atulkashyap@gmail.com>
2024-06-12 11:28:31 +0530

    Added Zegocloud in project

--- src/components/VideoPage.jsx
+++ src/components/VideoPage.jsx
@@ -1,15 +1,42 @@
-// import React from 'react'
+// // import React from 'react'
 
-import {useParams} from "react-router-dom"
+import { useParams } from "react-router-dom";
+import { ZegoUIKitPrebuilt } from '@zegocloud/zego-uikit-prebuilt';
+import { APP_ID, SERVER_SECRET } from "../constants";
 
 const VideoPage = () => {
-    const {id} = useParams()
+  const { id } = useParams();
+  const roomID = id;
 
+  const myMeeting = async (element) => {
+    // generate Kit Token
+    const appID = APP_ID;
+    const serverSecret = SERVER_SECRET;
+    const kitToken = ZegoUIKitPrebuilt.generateKitTokenForTest(appID, serverSecret, roomID, Date.now().toString(), "AtulKashyap");
 
+    // Create instance object from Kit Token.
+    const zp = ZegoUIKitPrebuilt.create(kitToken);
+    // start the call
+    zp.joinRoom({
+      container: element,
+      sharedLinks: [
+        {
+          name: 'Personal link',
+          url:
+            window.location.protocol + '//' +
+            window.location.host + window.location.pathname +
+            '?roomID=' + roomID,
+        },
+      ],
+      scenario: {
+        mode: ZegoUIKitPrebuilt.GroupCall, // To implement 1-on-1 calls, modify the parameter here to [ZegoUIKitPrebuilt.OneONoneCall]. to implement one-to-one call you have to write OneOnOneCall in place of GroupCall
+      },
+    });
+  };
 
   return (
-    <div>VideoPage {id}</div>
-  )
-}
+    <div ref={myMeeting}>VideoPage {id}</div>
+  );
+};
 
-export default VideoPage
+export default VideoPage;
